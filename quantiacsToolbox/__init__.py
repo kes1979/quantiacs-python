facb0eb93f0f9f0717099ae247e6d81a452d6410
Michal Tondyra <michal.tondyra@superfund.com>
2018-04-26 15:33:13 +0200

    ccc

--- quantiacsToolbox/__init__.py
+++ quantiacsToolbox/__init__.py
@@ -3,3 +3,4 @@ moduleList = [i.key for i in pip.get_installed_distributions()]
 
 if 'quantiacstoolbox' in moduleList:
     from .quantiacsToolbox import runts, loadData, plotts, stats, submit, computeFees, updateCheck
+#
